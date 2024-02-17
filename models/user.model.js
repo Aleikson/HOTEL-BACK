5c7acf2e2507140b6eb67419408613b11575e6b7
Aleikson <aleiksonsilva@hotmail.com>
2024-02-17 10:42:08 -0300

    Avatar default

--- models/user.model.js
+++ models/user.model.js
@@ -17,7 +17,7 @@ const userSchme = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: ''
+        default: 'https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352156-stock-illustration-default-placeholder-profile-icon.jpg'
     },
 }, { timestamps: true });
 
