64c497d1f82826a2014c561daa8e24923ea67a22
Aleikson <aleiksonsilva@hotmail.com>
2024-02-28 15:08:42 -0300

    Feat delete/update listing

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -1,9 +1,57 @@
 import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (request, response, next) => {
     try {
         const listing = await Listing.create(request.body);
         return response.status(201).json(listing);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteListing = async (request, response, next) => {
+    const listing = await Listing.findById(request.params.id);
+    if (!listing) {
+        return next(errorHandler(401), 'You can only delete your own listings!');
+    }
+    if (request.user.id !== listing.userRef) {
+    }
+    try {
+        await Listing.findByIdAndDelete(request.params.id);
+        response.status(200).json('Listing has been deleted');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateListing = async (request, response, next) => {
+    const listing = await Listing.findById(request.params.id);
+    if (!listing) {
+        return next(errorHandler(404), 'Listing not found!');
+    }
+    if (request.params.id === listing.userRef) {
+        return next(errorHandler(401), 'You can only update your own listings!');
+    }
+    try {
+        const listingUpdate = await Listing.findByIdAndUpdate(
+            request.params.id,
+            request.body,
+            { new: true }
+        );
+        response.status(200).json(listingUpdate);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getListing = async (request, response, next) => {
+    try {
+        const listing = await Listing.findById(request.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+        response.status(200).json(listing);
     } catch (error) {
         next(error);
     }
