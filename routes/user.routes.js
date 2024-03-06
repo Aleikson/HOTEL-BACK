462c9413c9577a0804c9f7613023c543c7bf094f
Aleikson <aleiksonsilva@hotmail.com>
2024-03-05 21:28:08 -0300

    Feat get user api

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,8 @@ import {
   deleteUser,
   test,
   updateUser,
-  getUserListings
+  getUserListings,
+  getUser
 } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
@@ -13,5 +14,6 @@ router.get('/test', test);
 router.post('/update/:id', verifyUser, updateUser);
 router.delete('/delete/:id', verifyUser, deleteUser);
 router.get('/listings/:id', verifyUser, getUserListings)
+router.get('/:id', verifyUser, getUser)
 
 export default router;
